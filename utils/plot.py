a1473d59349b8a1e33fbbc621210de041dea9721
albindahlberg <albin.c.dahlberg@gmail.com>
2024-11-15 14:25:28 +0100

    banana plotting function added

--- utils/plot.py
+++ utils/plot.py
@@ -2,6 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+def plot_bananas(x, y, alpha=0.3, s=0.1):
+    """ Plots bananas of ToF-ERDA experiments """
+    plt.figure(figsize=(10,8))
+    plt.scatter(x, y, alpha=alpha, s=s)
+    plt.xlabel('ToF (channel)')
+    plt.ylabel('energy (channel)')
+    plt.tight_layout()
+
 def make_palette(K, cmap=plt.cm.rainbow):
     """ Creates an evenly spaced color map with K colors """
     indices = np.linspace(0, 1, K)
